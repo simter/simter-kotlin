26741c1e4c5655e6ce7d07a952b3d7189f243ca5
RJ <rongjihuang@gmail.com>
2022-09-14 14:42:38 +0800

    Add json extension for convert JsonElement to native value

--- src/main/kotlin/tech/simter/kotlin/serialization/JsonExtension.kt
+++ src/main/kotlin/tech/simter/kotlin/serialization/JsonExtension.kt
@@ -2,6 +2,12 @@ package tech.simter.kotlin.serialization
 
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.*
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
 
 /**
  * Encode [value] to json-string without [excludeProperties].
@@ -28,5 +34,202 @@ fun JsonElement.excludeProperties(vararg excludeProperties: String): JsonElement
     is JsonObject -> JsonObject(this.filterNot { excludeProperties.contains(it.key) })
     is JsonArray -> JsonArray(this.map { it.excludeProperties(*excludeProperties) })
     else -> this
+  }
+}
+
+/** Refactor this [JsonObject] to without [excludeProperties] */
+fun JsonObject.excludeProperties(vararg excludeProperties: String): JsonObject {
+  return if (excludeProperties.isEmpty()) this
+  else JsonObject(this.filterNot { excludeProperties.contains(it.key) })
+}
+
+/** Refactor this [JsonArray] item to without [excludeProperties] */
+fun JsonArray.excludeProperties(vararg excludeProperties: String): JsonArray {
+  return if (excludeProperties.isEmpty()) this
+  else JsonArray(this.map { it.excludeProperties(*excludeProperties) })
+}
+
+/**
+ * Returns content of the current element as short
+ * @throws NumberFormatException if current element is not a valid representation of number
+ */
+val JsonPrimitive.short: Short get() = content.toShort()
+
+/**
+ * Returns content of the current element as short or `null` if current element is not a valid representation of number
+ */
+val JsonPrimitive.shortOrNull: Short? get() = content.toShortOrNull()
+
+/**
+ * Returns content of the current element as [BigDecimal]
+ * @throws NumberFormatException if current element is not a valid representation of number
+ */
+val JsonPrimitive.bigDecimal: BigDecimal get() = BigDecimal(content)
+
+/**
+ * Returns content of the current element as [BigDecimal]
+ * @throws NumberFormatException if current element is not a valid representation of number
+ */
+val JsonPrimitive.bigDecimalOrNull: BigDecimal?
+  get() = if (this is JsonNull || content.isEmpty()) null
+  else BigDecimal(content)
+
+/**
+ * Returns content of the current element as ISO format [LocalDate]
+ * @throws DateTimeParseException if the content cannot be parsed
+ */
+val JsonPrimitive.isoLocalDate: LocalDate get() = LocalDate.parse(content, DateTimeFormatter.ISO_LOCAL_DATE)
+
+/**
+ * Returns content of the current element as ISO format [LocalDate]
+ * @throws DateTimeParseException if the content cannot be parsed
+ */
+val JsonPrimitive.isoLocalDateOrNull: LocalDate?
+  get() = if (this is JsonNull || content.isEmpty()) null
+  else LocalDate.parse(content, DateTimeFormatter.ISO_LOCAL_DATE)
+
+/**
+ * Returns content of the current element as ISO format [LocalDate]
+ * @throws DateTimeParseException if the content cannot be parsed
+ */
+val JsonPrimitive.isoLocalTime: LocalTime get() = LocalTime.parse(content, DateTimeFormatter.ISO_LOCAL_TIME)
+
+/**
+ * Returns content of the current element as ISO format [LocalTime]
+ * @throws DateTimeParseException if the content cannot be parsed
+ */
+val JsonPrimitive.isoLocalTimeOrNull: LocalTime?
+  get() = if (this is JsonNull || content.isEmpty()) null
+  else LocalTime.parse(content, DateTimeFormatter.ISO_LOCAL_TIME)
+
+/**
+ * Returns content of the current element as ISO format [LocalDateTime]
+ * @throws DateTimeParseException if the content cannot be parsed
+ */
+val JsonPrimitive.isoLocalDateTime: LocalDateTime
+  get() = LocalDateTime.parse(content, DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+
+/**
+ * Returns content of the current element as iso format [LocalDateTime]
+ * @throws DateTimeParseException if the content cannot be parsed
+ */
+val JsonPrimitive.isoLocalDateTimeOrNull: LocalDateTime?
+  get() = if (this is JsonNull || content.isEmpty()) null
+  else LocalDateTime.parse(content, DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+
+private val commonLocalDateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+
+/**
+ * Returns content of the current element as ISO format [LocalDateTime]
+ * @throws DateTimeParseException if the content cannot be parsed
+ */
+val JsonPrimitive.commonLocalDateTime: LocalDateTime
+  get() = LocalDateTime.parse(content, commonLocalDateTimeFormatter)
+
+/**
+ * Returns content of the current element as ISO format [LocalDateTime]
+ * @throws DateTimeParseException if the content cannot be parsed
+ */
+val JsonPrimitive.commonLocalDateTimeOrNull: LocalDateTime?
+  get() = if (this is JsonNull || content.isEmpty()) null
+  else LocalDateTime.parse(content, commonLocalDateTimeFormatter)
+
+val TO_LONG_MAPPER: (value: JsonElement) -> Long? = { value -> value.jsonPrimitive.longOrNull }
+val TO_INT_MAPPER: (value: JsonElement) -> Int? = { value -> value.jsonPrimitive.intOrNull }
+val TO_SHORT_MAPPER: (value: JsonElement) -> Short? = { value -> value.jsonPrimitive.shortOrNull }
+val TO_BIG_DECIMAL_MAPPER: (value: JsonElement) -> BigDecimal? =
+  { value -> value.jsonPrimitive.bigDecimalOrNull }
+val TO_DOUBLE_MAPPER: (value: JsonElement) -> Double? = { value -> value.jsonPrimitive.doubleOrNull }
+val TO_FLOAT_MAPPER: (value: JsonElement) -> Float? = { value -> value.jsonPrimitive.floatOrNull }
+val TO_ISO_LOCAL_DATE_MAPPER: (value: JsonElement) -> LocalDate? =
+  { value -> value.jsonPrimitive.isoLocalDateOrNull }
+val TO_ISO_LOCAL_TIME_MAPPER: (value: JsonElement) -> LocalTime? =
+  { value -> value.jsonPrimitive.isoLocalTimeOrNull }
+val TO_ISO_LOCAL_DATE_TIME_MAPPER: (value: JsonElement) -> LocalDateTime? =
+  { value -> value.jsonPrimitive.isoLocalDateTimeOrNull }
+
+/**
+ * Convert the [JsonObject] instance to a [Map] with native value.
+ * Default [JsonPrimitive] value is auto convert to [String] type value.
+ */
+fun JsonObject.toNativeMap(
+  key: String = "",
+  defaultDecimalValueMapper: (value: JsonPrimitive) -> Number? = TO_BIG_DECIMAL_MAPPER,
+  defaultIntegerValueMapper: (value: JsonPrimitive) -> Number? = TO_LONG_MAPPER,
+  valueMapper: Map<String, (value: JsonElement) -> Any?> = emptyMap(),
+): Map<String, Any?> {
+  return this.entries.associate {
+    it.key to it.value.toNativeValue(
+      key = if (key.isEmpty()) it.key else "${key}.${it.key}",
+      valueMapper = valueMapper,
+      defaultIntegerValueMapper = defaultIntegerValueMapper,
+      defaultDecimalValueMapper = defaultDecimalValueMapper,
+    )
+  }
+}
+
+/**
+ * Convert the [JsonArray] instance to a [List] with native [Map] value.
+ * Default [JsonPrimitive] value is auto convert to [String] type value.
+ */
+fun <T> JsonArray.toNativeList(
+  key: String = "",
+  defaultDecimalValueMapper: (value: JsonPrimitive) -> Number? = TO_BIG_DECIMAL_MAPPER,
+  defaultIntegerValueMapper: (value: JsonPrimitive) -> Number? = TO_LONG_MAPPER,
+  valueMapper: Map<String, (value: JsonElement) -> Any?> = emptyMap(),
+): List<T> {
+  return this.map {
+    @Suppress("UNCHECKED_CAST")
+    it.toNativeValue(
+      key = key,
+      valueMapper = valueMapper,
+      defaultIntegerValueMapper = defaultIntegerValueMapper,
+      defaultDecimalValueMapper = defaultDecimalValueMapper,
+    ) as T
+  }
+}
+
+/**
+ * Convert the [JsonElement] instance to a native value.
+ */
+fun JsonElement.toNativeValue(
+  key: String,
+  defaultDecimalValueMapper: (value: JsonPrimitive) -> Number? = TO_BIG_DECIMAL_MAPPER,
+  defaultIntegerValueMapper: (value: JsonPrimitive) -> Number? = TO_LONG_MAPPER,
+  valueMapper: Map<String, (value: JsonElement) -> Any?> = emptyMap(),
+): Any? {
+  return if (valueMapper.containsKey(key)) valueMapper[key]!!.invoke(this)
+  else {
+    when (this) {
+      is JsonNull -> null
+      is JsonPrimitive -> {
+        // confirm string first
+        if (this.isString) return this.content
+
+        // try number and boolean
+        this.longOrNull?.let { return defaultIntegerValueMapper(this) }
+        this.doubleOrNull?.let { return defaultDecimalValueMapper(this) }
+        this.booleanOrNull?.let { return it }
+
+        // default string
+        this.content
+      }
+
+      is JsonArray -> this.map {
+        it.toNativeValue(
+          key = key,
+          valueMapper = valueMapper,
+          defaultDecimalValueMapper = defaultDecimalValueMapper,
+          defaultIntegerValueMapper = defaultIntegerValueMapper,
+        )
+      }
+
+      is JsonObject -> this.toNativeMap(
+        key = key,
+        valueMapper = valueMapper,
+        defaultDecimalValueMapper = defaultDecimalValueMapper,
+        defaultIntegerValueMapper = defaultIntegerValueMapper,
+      )
+    }
   }
 }
--- src/test/kotlin/tech/simter/kotlin/serialization/JsonExtensionTest.kt
+++ src/test/kotlin/tech/simter/kotlin/serialization/JsonExtensionTest.kt
@@ -1,10 +1,11 @@
 package tech.simter.kotlin.serialization
 
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.encodeToJsonElement
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.math.BigDecimal
 
 class JsonExtensionTest {
   @Serializable
@@ -15,7 +16,7 @@ class JsonExtensionTest {
     val b: Boolean
   )
 
-  private val json = Json {}
+  val json = Json {}
   private val bean = Bean(s = "s", n = null, i = 1, b = true)
   private val beans = listOf(Bean(s = "s", n = null, i = 1, b = true))
 
@@ -51,5 +52,84 @@ class JsonExtensionTest {
     assertThat(json.encodeToString(beans)).isEqualTo("""[{"s":"s","n":null,"i":1,"b":true}]""")
     assertThat(json.encodeToString(beans, "s")).isEqualTo("""[{"n":null,"i":1,"b":true}]""")
     assertThat(json.encodeToString(beans, "s", "n")).isEqualTo("""[{"i":1,"b":true}]""")
+  }
+
+  @Test
+  fun `jsonObject to native map - default integer to Long, decimal to BigDecimal`() {
+    val j = json.decodeFromString<JsonObject>(
+      """
+      {"n":null,"s":"s","b":true,"i1":1,"i2":1.23,"items":["a", "b"]}
+      """.trimIndent()
+    )
+    val m = j.toNativeMap()
+    assertThat(m).isEqualTo(
+      mapOf(
+        "n" to null,
+        "s" to "s",
+        "b" to true,
+        "i1" to 1L,
+        "i2" to BigDecimal("1.23"),
+        "items" to listOf("a", "b")
+      )
+    )
+  }
+
+  @Test
+  fun `jsonObject to native map - integer to Int, decimal to double`() {
+    val j = json.decodeFromString<JsonObject>("""{"n":null,"s":"s","b":true,"i1":1,"i2":1.23}""")
+    val m = j.toNativeMap(
+      defaultIntegerValueMapper = TO_INT_MAPPER,
+      defaultDecimalValueMapper = TO_DOUBLE_MAPPER,
+    )
+    assertThat(m).isEqualTo(
+      mapOf(
+        "n" to null,
+        "s" to "s",
+        "b" to true,
+        "i1" to 1,
+        "i2" to 1.23,
+      )
+    )
+  }
+
+  @Test
+  fun `jsonObject to native map - with value mapper 1`() {
+    val j = json.decodeFromString<JsonObject>("""{"n":null,"s":"s","b":true,"i1":1,"i2":1.23}""")
+    val m = j.toNativeMap(valueMapper = mapOf(
+      "n" to { "hello" },
+      "i1" to { it.jsonPrimitive.int + 1 }
+    ))
+    assertThat(m).isEqualTo(
+      mapOf(
+        "n" to "hello",
+        "s" to "s",
+        "b" to true,
+        "i1" to 2,
+        "i2" to BigDecimal("1.23"),
+      )
+    )
+  }
+
+  @Test
+  fun `jsonObject to native map - with value mapper 2`() {
+    val j = json.decodeFromString<JsonObject>("""{"n":null,"items":["a","b"]}""")
+    val m = j.toNativeMap(valueMapper = mapOf("items" to { listOf("c") }))
+    assertThat(m).isEqualTo(
+      mapOf(
+        "n" to null,
+        "items" to listOf("c")
+      )
+    )
+  }
+
+  @Test
+  fun `jsonObject to native map - with value mapper 3`() {
+    val j = json.decodeFromString<JsonObject>("""{"items":[{"i":1,"s":"s"}]}""")
+    val m = j.toNativeMap(valueMapper = mapOf("items.s" to { "${it.jsonPrimitive.content}.m" }))
+    assertThat(m).isEqualTo(
+      mapOf(
+        "items" to listOf(mapOf("i" to 1L, "s" to "s.m"))
+      )
+    )
   }
 }
